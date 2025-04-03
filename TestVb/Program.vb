552e872115556ea8fa31f2f5cc940b88d70e71d8
akhilesh.aviyaan@gmail.com <akhilesh.aviyaan@gmail.com>
2025-04-03 09:03:26 +0545

    needs dubugging

--- TestVb/Program.vb
+++ TestVb/Program.vb
@@ -8,8 +8,8 @@ Public Class ChainageElevationGroup
 	Public Chainage As Double
 End Class
 Public Class Program
-	Public paperLength = 4
-	Public paperHeight = 10
+	Public Shared paperLength = 4
+	Public Shared paperHeight = 10
 	Public Shared Sub Main()
 		Dim group1 = New List(Of ChainageElevationGroup)()
 		Dim seperatedGroup = New List(Of List(Of ChainageElevationGroup))()
@@ -27,29 +27,53 @@ Public Class Program
 	End Sub
 	Public Shared Sub SeperateByPaper(group1 As List(Of ChainageElevationGroup), ByRef seperatedgroup As List(Of List(Of ChainageElevationGroup)))
 
-		Dim lastIndex = group1.Count()
-		Dim minChainage As Double
-		Dim maxChainage As Double
-		Dim minElevation As Double
-		Dim maxElevation As Double
-		minChainage = group1(0).Chainage
-		minChainage = group1(0).Chainage
-		minElevation = group1(0).Elevation
-		minElevation = group1(0).Elevation
-		If group1.Count < 2 Then Return
+		Dim lastIndex = group1.Count() - 1
 
-		Dim startIndex As Integer = 0
+		For i As Integer = 0 To lastIndex Step 1
+			Dim Index = Math.Min(ChainageListConsideringPaperHeight(group1, i), ChainageListConsideringPaperLength(group1, i))
+			Dim sg = group1.GetRange(i, Index + 1 - i)
+			seperatedgroup.Add(sg)
+			i = Index
+		Next
 
-		Dim listFromChainge = New List(Of ChainageElevationGroup)
+	End Sub
+	Public Shared Function ChainageListConsideringPaperHeight(group1 As List(Of ChainageElevationGroup), startIndex As Integer) As Integer
+		If group1.Count() < 2 Then Return startIndex
+		Dim minElevation = group1(startIndex).Elevation
+		Dim maxElevation = group1(startIndex).Elevation
+		Dim lastIndex = group1.Count() - 1
+		Dim EndIndex As Integer
 		For i As Integer = startIndex To lastIndex - 1 Step 1
-			maxChainage = group1(i).Chainage
-			listFromChainge.Add(group1(i))
-			If maxChainage > paperLength Then
+			Dim e1 = group1(i).Elevation
+			Dim e2 = group1(i + 1).Elevation
+			If e2 > e1 Then
+				maxElevation = e2
+			Else
+				minElevation = e2
+			End If
+			Dim DiffElevation = Math.Abs(maxElevation - minElevation) 'After this scale it
+			If maxElevation < paperLength Then
+				EndIndex = i
 				Exit For
 			End If
 		Next
-	End Sub
-	Public Function ChainageListConsideringPaperLength(group1 As List(Of ChainageElevationGroup), startIndex As Integer)
+		Return (EndIndex)
+	End Function
+	Public Shared Function ChainageListConsideringPaperLength(group1 As List(Of ChainageElevationGroup), startIndex As Integer) As Integer
+		If group1.Count() < 2 Then Return startIndex
+		Dim minChainage = group1(startIndex).Chainage
+		Dim maxChainage = group1(startIndex).Chainage
+		Dim lastIndex = group1.Count() - 1
+		Dim EndIndex As Integer
+		For i As Integer = startIndex To lastIndex Step 1
+			maxChainage = group1(i).Chainage
+			Dim diffChainage = maxChainage - minChainage 'After this scale it
+			If diffChainage < paperLength Then
+				EndIndex = i
+				Exit For
+			End If
+		Next
+		Return (EndIndex)
 	End Function
 End Class
 
